7d4ad164ee43999eeb229c2726bf6b43a381af93
David <davidellis1020@gmail.com>
2021-01-26 16:46:12 -0800

    Set up CodeGrade

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ import { fifaData } from './fifa.js';
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Practice accessing data by console.log-ing the following pieces of data note, you may want to filter the data first 😉*/
+const finals2014 = fifaData.filter(function(item){
+    return item.Year === 2014 && item.Stage === 'Final';
+});
+
+//console.log(finals2014);
 
 //(a) Home Team name for 2014 world cup final
+console.log(finals2014[0]['Home Team Name']);
 
 //(b) Away Team name for 2014 world cup final
 
@@ -24,10 +30,15 @@ Use getFinals to do the following:
 hint - you should be looking at the stage key inside of the objects
 */
 
-function getFinals(/* code here */) {
+function getFinals(data) {
    /* code here */
+   const mappedFinalists = data.filter((item) => {
+       return item.Stage === 'Final';
+   });
+   return mappedFinalists;
 }
-
+//console.log(getFinals(fifaData));
+//console.log('Hello');
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
